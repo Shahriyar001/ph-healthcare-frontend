dde2f3e3dff87c91011204aef630d2d76e9dd027
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-04-07 23:56:31 +0600

    added footer

--- src/componants/Shared/Footer/Footer.tsx
+++ src/componants/Shared/Footer/Footer.tsx
@@ -1,8 +1,21 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <Box bgcolor="rgb(17, 26, 34) py-5">
+      <Container>
+        <Stack direction="row" justifyContent="center" gap={4}>
+          <Typography color="#fff" component={Link} href="/consultation">
+            Consultation
+          </Typography>
+          <Typography color="#fff">Health Plans</Typography>
+          <Typography color="#fff">Medicine</Typography>
+          <Typography color="#fff">Diognostics</Typography>
+          <Typography color="#fff">NGOs</Typography>
+        </Stack>
+      </Container>
+    </Box>
   );
 };
 
