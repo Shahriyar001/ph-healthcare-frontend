f99617e4ff490e37ea56a7117b325a907dd50623
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-04-09 22:48:41 +0600

    complite homepahe hero section

--- src/componants/UI/HomePage/HeroSection/HeroSection.tsx
+++ src/componants/UI/HomePage/HeroSection/HeroSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import Image from "next/image";
 import assets from "@/assets";
 
@@ -27,9 +27,112 @@ const HeroSection = () => {
         >
           <Image src={assets.svgs.grid} alt="grid" />
         </Box>
-        <Typography>Healther Hearts</Typography>
+        <Typography variant="h3" component="h1" fontWeight={600}>
+          Healther Hearts
+        </Typography>
+        <Typography variant="h3" component="h1" fontWeight={600}>
+          come From
+        </Typography>
+        <Typography
+          color="primary.main"
+          variant="h3"
+          component="h1"
+          fontWeight={600}
+        >
+          Preventive Care
+        </Typography>
+        <Typography
+          variant="h6"
+          component="p"
+          fontWeight={400}
+          sx={{ my: 4, width: "50%" }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+          eligendi laudantium, vel eius dignissimos et alias? Deleniti
+          voluptatum labore debitis laboriosam,
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 2,
+          }}
+        >
+          <Button>Make Appointment</Button>
+          <Button variant="outlined">Contact Us</Button>
+        </Box>
       </Box>
-      <Box>Right</Box>
+      <Box
+        sx={{
+          p: 1,
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          position: "relative",
+          mt: 0,
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            left: "200px",
+            top: "-30px",
+          }}
+        >
+          <Image src={assets.svgs.arrow} alt="arrow" width={100} height={100} />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 2,
+          }}
+        >
+          <Box mt={4}>
+            <Image
+              src={assets.images.doctor1}
+              width={240}
+              height={380}
+              alt="doctor1"
+            />
+          </Box>
+          <Box>
+            <Image
+              src={assets.images.doctor2}
+              width={240}
+              height={380}
+              alt="doctor2"
+            />
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "220px",
+            left: "150px",
+          }}
+        >
+          <Image
+            src={assets.images.doctor3}
+            height={240}
+            width={240}
+            alt="doctor3"
+          />
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "-50px",
+            right: 0,
+            zIndex: "-1",
+          }}
+        >
+          <Image
+            src={assets.images.stethoscope}
+            height={180}
+            width={180}
+            alt="doctor3"
+          />
+        </Box>
+      </Box>
     </Container>
   );
 };
