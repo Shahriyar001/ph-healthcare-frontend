48ed144257257faeaf0bd828a8c832cdc244f41d
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-04-15 23:51:23 +0600

    Create Top Rated Doctors Section With Data Fetching

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/componants/UI/HomePage/HeroSection/HeroSection";
 import Specialist from "@/componants/UI/HomePage/Specialist/Specialist";
+import TopRatedDoctors from "@/componants/UI/HomePage/TopRatedDoctors/TopRatedDoctors";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <Specialist />
+      <TopRatedDoctors />
     </>
   );
 };
