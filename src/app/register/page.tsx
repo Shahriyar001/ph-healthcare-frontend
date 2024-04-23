f33540574801366eb08987f4f9191902315a386a
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-04-23 23:47:56 +0600

    Create Patient Register Page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,66 @@
+import {
+  Box,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import assets from "@/assets";
+
 const RegisterPage = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <Container
+      sx={{
+        padding: "50px",
+      }}
+    >
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Image src={assets.svgs.logo} width={50} height={50} alt="logo" />
+            </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Patient Register
+              </Typography>
+            </Box>
+          </Stack>
+          <Box>
+            <Grid container spacing={2} my={1}>
+              <Grid item md={12}>
+                <TextField
+                  label="Name"
+                  variant="outlined"
+                  size="small"
+                  fullWidth={true}
+                />
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
