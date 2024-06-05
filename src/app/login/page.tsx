0e50f6dd46766c554c45a45046657f2c9c0c8bb8
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-06-05 17:15:41 +0600

    Implementing Form Validation With Zod And ZodResolver

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,6 +20,12 @@ import { Router } from "next/router";
 import { useRouter } from "next/navigation";
 import PHForm from "@/componants/Forms/PHForm";
 import PHInput from "@/componants/Forms/PHInput";
+import { z } from "zod";
+
+export const validationSchema = z.object({
+  email: z.string().email("Please enter a valid email address!"),
+  password: z.string().min(6, "must be atlest 6 character"),
+});
 
 const LoginPage = () => {
   const router = useRouter();
