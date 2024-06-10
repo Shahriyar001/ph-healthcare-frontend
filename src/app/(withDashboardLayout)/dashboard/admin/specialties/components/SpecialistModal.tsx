8cc67f5dd9333f07f4ccc412a4f87285b5c9d250
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-06-10 23:39:37 +0600

    Using Form Component Inside Reusable Modal

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialistModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialistModal.tsx
@@ -1,5 +1,9 @@
+import PHFileUploader from "@/componants/Forms/PHFileUploader";
+import PHForm from "@/componants/Forms/PHForm";
+import PHInput from "@/componants/Forms/PHInput";
 import PHModal from "@/componants/Shared/PHModal/PHModal";
-import { TextField } from "@mui/material";
+import { Button, Grid } from "@mui/material";
+import { FieldValues } from "react-hook-form";
 
 type Tprops = {
   open: boolean;
@@ -7,9 +11,23 @@ type Tprops = {
 };
 
 const SpecialistModal = ({ open, setOpen }: Tprops) => {
+  const handleFormSubmit = (value: FieldValues) => {};
+
   return (
-    <PHModal open={open} setOpen={setOpen} title="Create Specialist">
-      <TextField />
+    <PHModal open={open} setOpen={setOpen} title="Create A New Specialist">
+      <PHForm onSubmit={handleFormSubmit}>
+        <Grid container spacing={2}>
+          <Grid item md={6}>
+            <PHInput name="title" label="Title" />
+          </Grid>
+          <Grid item md={6}>
+            <PHFileUploader />
+          </Grid>
+        </Grid>
+        <Button sx={{ mt: 1 }} type="submit">
+          Create
+        </Button>
+      </PHForm>
     </PHModal>
   );
 };
