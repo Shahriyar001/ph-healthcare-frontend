0ccdd7c85f4a25fe2588f059b627252bc89c3cf7
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-06-13 22:42:33 +0600

    update post method

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialistModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialistModal.tsx
@@ -6,6 +6,7 @@ import { useCreateSpecialtyMutation } from "@/redux/api/specialitiesApi";
 import { modifyPayload } from "@/ulils/modifyPayload";
 import { Button, Grid } from "@mui/material";
 import { FieldValues } from "react-hook-form";
+import { toast } from "sonner";
 
 type Tprops = {
   open: boolean;
@@ -19,8 +20,12 @@ const SpecialistModal = ({ open, setOpen }: Tprops) => {
     console.log(values);
     const data = modifyPayload(values);
     try {
-      const res = await createSpecialty(data);
+      const res = await createSpecialty(data).unwrap();
       console.log(res);
+      if (res?.id) {
+        toast.success("Specialty created successfully");
+        setOpen(false);
+      }
     } catch (err: any) {
       console.error(err.message);
     }
@@ -46,3 +51,52 @@ const SpecialistModal = ({ open, setOpen }: Tprops) => {
 };
 
 export default SpecialistModal;
+
+// import PHFileUploader from "@/components/Forms/PHFileUploader";
+// import PHForm from "@/components/Forms/PHForm";
+// import PHInput from "@/components/Forms/PHInput";
+// import PHModal from "@/components/Shared/PHModal/PHModal";
+// import { useCreateSpecialtyMutation } from "@/redux/api/specialitiesApi";
+// import { modifyPayload } from "@/utils/modifyPayload";
+// import { Button, Grid } from "@mui/material";
+// import { FieldValues } from "react-hook-form";
+
+// type Tprops = {
+//   open: boolean;
+//   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+// };
+
+// const SpecialistModal = ({ open, setOpen }: Tprops) => {
+//   const [createSpecialty] = useCreateSpecialtyMutation();
+
+//   const handleFormSubmit = async (values: FieldValues) => {
+//     console.log(values);
+//     const data = modifyPayload(values);
+//     try {
+//       const res = await createSpecialty(data);
+//       console.log(res);
+//     } catch (err: any) {
+//       console.error(err.message);
+//     }
+//   };
+
+//   return (
+//     <PHModal open={open} setOpen={setOpen} title="Create A New Specialist">
+//       <PHForm onSubmit={handleFormSubmit}>
+//         <Grid container spacing={2}>
+//           <Grid item md={6}>
+//             <PHInput name="title" label="Title" />
+//           </Grid>
+//           <Grid item md={6}>
+//             <PHFileUploader name="file" label="Upload File" />
+//           </Grid>
+//         </Grid>
+//         <Button sx={{ mt: 1 }} type="submit">
+//           Create
+//         </Button>
+//       </PHForm>
+//     </PHModal>
+//   );
+// };
+
+// export default SpecialistModal;
