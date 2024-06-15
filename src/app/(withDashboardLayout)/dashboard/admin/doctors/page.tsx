0f43c24dfa6445ccd3e8d169b411292d41ebdf7d
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-06-15 23:58:56 +0600

    add doctor modal

--- src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
@@ -1,8 +1,13 @@
+import { Box, Button, Stack, TextField } from "@mui/material";
+
 const DoctorsPage = () => {
   return (
-    <div>
-      <h1>Doctors Page</h1>
-    </div>
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Button>Create New Doctor</Button>
+        <TextField size="small" placeholder="search doctor" />
+      </Stack>
+    </Box>
   );
 };
 
