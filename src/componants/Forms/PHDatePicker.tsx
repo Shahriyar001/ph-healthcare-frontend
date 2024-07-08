d81c1c5d2ac19e9be9772a4996e095e6b74261d4
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-07-08 23:56:54 +0600

    added IDatePicher interface

--- src/componants/Forms/PHDatePicker.tsx
+++ src/componants/Forms/PHDatePicker.tsx
@@ -2,12 +2,43 @@ import dayjs from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { SxProps, useFormControl } from "@mui/material";
 
-const PHDatePicker = () => {
+interface IDatePicker {
+  name: string;
+  size?: "small" | "medium";
+  label?: string;
+  required: boolean;
+  fullWidth?: boolean;
+  sx?: SxProps;
+}
+
+const PHDatePicker = ({
+  name,
+  size = "small",
+  label,
+  required,
+  fullWidth = true,
+  sx,
+}: IDatePicker) => {
+  const { control } = useFormControl();
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DesktopDatePicker defaultValue={dayjs(new Date().toDateString())} />
-    </LocalizationProvider>
+    <Controller
+      name={name}
+      control={control}
+      defaultValue={dayjs(new Date().toDateString())}
+      render={({ field: { onChange, value, ...field } }) => {
+        return (
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              {...field}
+              onChange={(date) => onChange(onChange)}
+              value={value || Date.now()}
+            />
+          </LocalizationProvider>
+        );
+      }}
+    />
   );
 };
 
