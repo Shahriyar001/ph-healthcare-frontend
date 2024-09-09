7e4c22c61ba78d7bbd1374425d75fdce732cb0cf
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-09-09 23:58:27 +0600

    added modal in doctorschedulemodal

--- src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
@@ -1,7 +1,11 @@
+import PHModal from "@/componants/Shared/PHModal/PHModal";
 import React from "react";
 
-const DoctorScheduleModal = () => {
-  return <div></div>;
+const DoctorScheduleModal = ({ open, setOpen }: TProps) => {
+  return;
+  <PHModal open={open} setOpen={setOpen} title="Create Doctor Schedule">
+    <h1>ABC</h1>
+  </PHModal>;
 };
 
 export default DoctorScheduleModal;
