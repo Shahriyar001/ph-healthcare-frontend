174a1eebe633cb43ac0cbbb2bd59648569f29856
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-09-11 17:50:22 +0600

    Implementing Doctor Scheduling Feature with Date Picker

--- src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
@@ -1,11 +1,64 @@
-import PHModal from "@/componants/Shared/PHModal/PHModal";
-import React from "react";
+// import PHModal from "@/componants/Shared/PHModal/PHModal";
+// import React, { useState } from "react";
+// import dayjs, { Dayjs } from "dayjs";
+// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+// import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+// import { TabProps } from "@mui/material";
+// // import { Dayjs } from "dayjs";
 
-const DoctorScheduleModal = ({ open, setOpen }: TProps) => {
-  return;
-  <PHModal open={open} setOpen={setOpen} title="Create Doctor Schedule">
-    <h1>ABC</h1>
-  </PHModal>;
+// const DoctorScheduleModal = ({ open, setOpen }: TabProps) => {
+//   const [selectedDate, setSeclectedDate] = useState(
+//     dayjs(new Date()).toISOString()
+//   );
+//   return
+//   <PHModal open={open} setOpen={setOpen} title="Create Doctor Schedule">
+//     <LocalizationProvider dateAdapter={AdapterDayjs}>
+//       <DatePicker
+//         label="Controlled picker"
+//         value={dayjs(selectedDate)}
+//         onChange={(newValue) => setSeclectedDate(dayjs(newValue).toISOString())}
+//       />
+//     </LocalizationProvider>
+//   </PHModal>;
+
+// };
+
+// export default DoctorScheduleModal;
+
+import PHModal from "@/componants/Shared/PHModal/PHModal";
+import React, { useState } from "react";
+import dayjs from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
+type DoctorScheduleModalProps = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const DoctorScheduleModal: React.FC<DoctorScheduleModalProps> = ({
+  open,
+  setOpen,
+}) => {
+  const [selectedDate, setSeclectedDate] = useState(
+    dayjs(new Date()).toISOString()
+  );
+  console.log(selectedDate);
+  return (
+    <PHModal open={open} setOpen={setOpen} title="Create Doctor Schedule">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          label="Controlled picker"
+          value={dayjs(selectedDate)}
+          onChange={(newValue) =>
+            setSeclectedDate(dayjs(newValue).toISOString())
+          }
+        />
+      </LocalizationProvider>
+    </PHModal>
+  );
 };
 
 export default DoctorScheduleModal;
